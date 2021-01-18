afada2ddc6c66b80c78d34c6e3f910cc629ce43f
WooJunKang <woojiworld@gmail.com>
2021-01-18 21:00:13 +0900

    feat: update todo-list

---
+++ to-do-app/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data( ){
+    return {
+      todos: [
+        {
+          id:1,
+          title: 'todo one',
+          completed: false
+        },
+        {
+          id:2,
+          title: 'todo two',
+          completed: false
+        },
+        {
+          id:3,
+          title: 'todo three',
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      // console.log(id)
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
+  },
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
